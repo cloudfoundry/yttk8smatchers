9d2efe78ce99fa0ec814843b59ee557588f94fca
Dennis Leon <leonde@vmware.com>
2020-09-25 12:08:18 -0700

    Allow making assertions on a deployment.Spec
    Authored-by: Dennis Leon <leonde@vmware.com>

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module code.cloudfoundry.org/yttk8smatchers
 go 1.14
 
 require (
-	github.com/onsi/ginkgo v1.13.0
+	github.com/benjamintf1/unmarshalledmatchers v1.0.0
+	github.com/onsi/ginkgo v1.14.0
 	github.com/onsi/gomega v1.10.1
 	istio.io/client-go v0.0.0-20200814134724-bcbf0ed82b30
 	k8s.io/api v0.18.8
 	k8s.io/apiextensions-apiserver v0.18.8
 	k8s.io/apimachinery v0.18.8
 	k8s.io/client-go v11.0.0+incompatible
+	sigs.k8s.io/yaml v1.2.0
 )
--- matchers/with_deployment_matcher.go
+++ matchers/with_deployment_matcher.go
@@ -2,16 +2,20 @@ package matchers
 
 import (
 	"fmt"
+	"github.com/benjamintf1/unmarshalledmatchers"
 	"github.com/onsi/gomega/format"
 	"github.com/onsi/gomega/types"
 	appsv1 "k8s.io/api/apps/v1"
+	"sigs.k8s.io/yaml"
 )
 
 type WithDeploymentMatcher struct {
 	name, namespace, errorMsg, errorMsgNotted string
 	matcher                                   types.GomegaMatcher
 	metas                                     []types.GomegaMatcher
+	specYaml                                  string
 	failedMatcher                             types.GomegaMatcher
+	failedMatcherActual                       interface{}
 }
 
 func WithDeployment(name, ns string) *WithDeploymentMatcher {
@@ -22,7 +26,13 @@ func WithDeployment(name, ns string) *WithDeploymentMatcher {
 	return &WithDeploymentMatcher{name: name, metas: metas}
 }
 
+func (matcher *WithDeploymentMatcher) WithSpecYaml(yaml string) *WithDeploymentMatcher {
+	matcher.specYaml = yaml
+	return matcher
+}
+
 func (matcher *WithDeploymentMatcher) Match(actual interface{}) (bool, error) {
+	matcher.failedMatcherActual = actual
 	docsMap, ok := actual.(map[string]interface{})
 	if !ok {
 		return false, fmt.Errorf("YAMLDocument must be passed a map[string]interface{}. Got\n%s", format.Object(actual, 1))
@@ -42,27 +52,43 @@ func (matcher *WithDeploymentMatcher) Match(actual interface{}) (bool, error) {
 			return ok, err
 		}
 	}
+
+	if matcher.specYaml != "" {
+		deploymentSpecYaml, err := yaml.Marshal(typedDeployment.Spec)
+		if err != nil {
+			return false, err
+		}
+
+		yamlMatcher := unmarshalledmatchers.ContainUnorderedYAML(matcher.specYaml)
+		ok, err = yamlMatcher.Match(deploymentSpecYaml)
+		if !ok || err != nil {
+			matcher.failedMatcher = yamlMatcher
+			matcher.failedMatcherActual = deploymentSpecYaml
+			return ok, err
+		}
+	}
+
 	return true, nil
 }
 
 func (matcher *WithDeploymentMatcher) FailureMessage(actual interface{}) string {
 	if matcher.failedMatcher == nil {
 		msg := fmt.Sprintf(
-			"FailureMessage: A statefulset with name %q doesnt exist",
+			"FailureMessage: A deployment with name %q doesnt exist",
 			matcher.name,
 		)
 		return msg
 	}
-	return matcher.failedMatcher.FailureMessage(actual)
+	return matcher.failedMatcher.FailureMessage(matcher.failedMatcherActual)
 }
 
 func (matcher *WithDeploymentMatcher) NegatedFailureMessage(actual interface{}) string {
 	if matcher.failedMatcher == nil {
 		msg := fmt.Sprintf(
-			"FailureMessage: A statefulset with name %q exists",
+			"FailureMessage: A deployment with name %q exists",
 			matcher.name,
 		)
 		return msg
 	}
-	return matcher.failedMatcher.NegatedFailureMessage(actual)
+	return matcher.failedMatcher.NegatedFailureMessage(matcher.failedMatcherActual)
 }
