1ce6971c624ca756a8c7d1c2b264c3b165894890
Andrew Costa <ancosta@vmware.com>
2020-11-05 16:32:11 -0800

    Add a CRD matcher
    [#175423840](https://www.pivotaltracker.com/story/show/175423840)
    
    Co-authored-by: Eric Promislow <epromislow@suse.com>

--- go.mod
+++ go.mod
@@ -9,9 +9,9 @@ require (
 	github.com/onsi/gomega v1.10.1
 	github.com/otiai10/copy v1.2.0
 	istio.io/client-go v0.0.0-20200814134724-bcbf0ed82b30
-	k8s.io/api v0.18.8
-	k8s.io/apiextensions-apiserver v0.18.8
-	k8s.io/apimachinery v0.18.8
+	k8s.io/api v0.18.9
+	k8s.io/apiextensions-apiserver v0.18.9
+	k8s.io/apimachinery v0.18.9
 	k8s.io/client-go v11.0.0+incompatible
 	sigs.k8s.io/yaml v1.2.0
 )
--- matchers/decoder.go
+++ matchers/decoder.go
@@ -5,6 +5,7 @@ import (
 	networkingv1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	networkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	securityv1beta1 "istio.io/client-go/pkg/apis/security/v1beta1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 	scheme "k8s.io/client-go/kubernetes/scheme"
@@ -13,6 +14,7 @@ import (
 // Kubernetes CRDs should have a scheme that can be added here.
 func GetDecoder() runtime.Decoder {
 	apiextensionsv1beta1.AddToScheme(scheme.Scheme)
+	apiextensionsv1.AddToScheme(scheme.Scheme)
 	networkingv1alpha3.AddToScheme(scheme.Scheme)
 	networkingv1beta1.AddToScheme(scheme.Scheme)
 	securityv1beta1.AddToScheme(scheme.Scheme)
